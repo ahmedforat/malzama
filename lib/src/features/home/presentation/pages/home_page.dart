de1754f7da2d8b3a503349889f314b2db8a92882
karrar <k.mohammed1133@gmail.com>
2020-06-23 14:13:13 +0300

    OneSignal Integration

--- lib/src/features/home/presentation/pages/home_page.dart
+++ lib/src/features/home/presentation/pages/home_page.dart
@@ -1,27 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:malzama/src/core/api/contract_response.dart';
 import 'package:malzama/src/core/platform/services/dialog_services/dialog_service.dart';
 import 'package:malzama/src/core/platform/services/dialog_services/service_locator.dart';
 import 'package:malzama/src/features/home/usecases/log_out.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 import '../../../../core/platform/services/file_system_services.dart';
 import '../widgets/bottom_nav_bar_pages/user_profile/pages/user_profile.dart';
 import '../widgets/bottom_nav_bar_widget.dart';
 
+// OneSignal App ID
+// 50c8ad6e-b20b-4f8e-a71a-219c4f4ce74e
+// 50c8ad6e-b20b-4f8e-a71a-219c4f4ce74e
+
+
+// userid 
+// 6e45569e-cbc6-413e-b9d0-3504194649f3
+// 6e45569e-cbc6-413e-b9d0-3504194649f3
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   GlobalKey<ScaffoldState> scaffoldKey;
   TabController tabController;
   PageController pageController;
+  FlutterLocalNotificationsPlugin notificationsPlugin = new FlutterLocalNotificationsPlugin();
+
+  AndroidInitializationSettings androidInitializationSettings;
+  IOSInitializationSettings iOSSettings;
+  InitializationSettings initializationSettings;
+
+  Future <void>_onDidRecieveLocalNotification(int x,String y,String z,String zz){
+    print('on did recieved lcoal notificaiton callback');
+  }
+
+  Future<void> _initilizeLocalNotification()async{
+    androidInitializationSettings = new AndroidInitializationSettings('app_icon.jpg');
+    iOSSettings = new IOSInitializationSettings(onDidReceiveLocalNotification: _onDidRecieveLocalNotification);
+    initializationSettings = new InitializationSettings(androidInitializationSettings, iOSSettings);
+   await notificationsPlugin.initialize(initializationSettings,onSelectNotification: (val)async{
+      print('notification has been selected');
+    });
+  }
+
+  Future<void> showNotification({int channelID,String title,String body})async{
+    NotificationDetails notificationDetails ;
+    AndroidNotificationDetails androidNotificationDetails = new  AndroidNotificationDetails("Channel_ID", "Channel title", "Channel body",
+    priority: Priority.High,
+    importance: Importance.Max,
+      ticker:'test'
+    );
+    IOSNotificationDetails iosNotificationDetails = new IOSNotificationDetails();
+    notificationDetails = new NotificationDetails(androidNotificationDetails, iosNotificationDetails);
+
+    await notificationsPlugin.show(channelID,title,body,notificationDetails);
+  }
+  Future<void> _initializeOneSignal() async {
+    
+    OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+  
+
+
+    OneSignal.shared.init("50c8ad6e-b20b-4f8e-a71a-219c4f4ce74e", iOSSettings: {
+      OSiOSSettings.autoPrompt: false,
+      OSiOSSettings.inAppLaunchUrl: false
+    });
+    OneSignal.shared
+        .setInFocusDisplayType(OSNotificationDisplayType.notification);
+    OneSignal.shared.sendTag('major', 'web-developer');
+    OneSignal.shared.setSubscriptionObserver((OSSubscriptionStateChanges changes) {
+        print('=========================================');
+         print('UUID');
+        print(changes.to.userId);
+         print('=========================================');
+     });
+// The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+    await OneSignal.shared
+        .promptUserForPushNotificationPermission(fallbackToSettings: true);
+
+    OneSignal.shared
+        .setNotificationReceivedHandler((OSNotification notification) {
+          // do whatever the hell you wanna do in your life mother fucker
+          print('this is the title');
+          print(notification.androidNotificationId);
+          print('addtional data');
+          print(notification.payload.additionalData);
+           print('body');
+          print(notification.payload.body);
+
+            showNotification(channelID: 0,title:notification.payload.title,body:notification.payload.body,);
+           print('subtitle');
+          print(notification.payload.subtitle);
+
+           print('subtitle');
+        
+
+
+          
+          
+      print(notification.payload.title);
+    });
+  }
 
   @override
   void initState() {
     tabController = new TabController(vsync: this, length: 4);
     scaffoldKey = new GlobalKey<ScaffoldState>();
+
+    // Subscribe to OneSignal
+    _initializeOneSignal();
     super.initState();
   }
 
@@ -69,7 +161,12 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
         child: TabBarView(
           physics: AlwaysScrollableScrollPhysics(),
           controller: tabController,
-          children: <Widget>[getFirstWidget(), getSecondWidget(), getThirdWidget(), UserProfilePage()],
+          children: <Widget>[
+            getFirstWidget(),
+            getSecondWidget(),
+            getThirdWidget(),
+            UserProfilePage()
+          ],
         ),
       ),
       bottomNavigationBar: BottomNavigationBarWidget(
@@ -93,13 +190,15 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
           onPressed: () async {
             ContractResponse response = await AccessManager.signOut();
             if (response is AuthorizationBreaking) {
-              Navigator.of(context).pushNamedAndRemoveUntil('/signup-page', (route) => false);
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/signup-page', (route) => false);
             } else if (response is SnackBarException) {
               Scaffold.of(context).showSnackBar(SnackBar(
                 content: Text(response.message),
               ));
             } else {
-              Navigator.of(context).pushNamedAndRemoveUntil('/signup-page', (route) => false);
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/signup-page', (route) => false);
             }
           },
         ),
